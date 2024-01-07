57b6ee3b5425ea7b2f0eb8fd80148bed2282e625
orjujeng <orjujeng@hotmail.com>
2024-01-07 20:59:29 +0800

    change sh file dokcerfile

--- build_expose.sh
+++ build_expose.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 pwd
 sudo cd /webservice
-sudo docker build -t app:lastest .
+sudo mv Dockerfile dockerfile
+sudo docker build -t app:lastest -f ./dockerfile .
 sudo docker run -d -p 8080:8080 app:lastest
