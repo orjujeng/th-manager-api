96e9cd514ac419eb89be7acdb194047fa72bf60c
orjujeng <orjujeng@hotmail.com>
2024-01-07 22:24:38 +0800

    add redis config

---
+++ src/main/java/com/orjujeng/manager/config/SessionConfig.java
@@ -0,0 +1,19 @@
+package com.orjujeng.manager.config;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
+public class SessionConfig {
+
+    /**
+//     * 此配置可以关闭代码设置redis config的逻辑
+     * @return
+     */
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
+}
